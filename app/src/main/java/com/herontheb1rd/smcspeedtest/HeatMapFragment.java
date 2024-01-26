3e1800d362b18a9df75cfbae6b294a1c641d86ad
Heron Nalasa <herongabriel.nalasa@smc.pshs.edu.ph>
2024-01-26 22:19:08 +0800

    Renamed functions. Added dictionary of locations to array of LatLngs for polygon positioning.

--- app/src/main/java/com/herontheb1rd/smcspeedtest/HeatMapFragment.java
+++ app/src/main/java/com/herontheb1rd/smcspeedtest/HeatMapFragment.java
@@ -2,11 +2,6 @@ package com.herontheb1rd.smcspeedtest;
 
 import android.os.Bundle;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import androidx.preference.PreferenceManager;
-
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -18,17 +13,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.maps.android.heatmaps.HeatmapTileProvider;
-import com.google.maps.android.heatmaps.WeightedLatLng;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -41,11 +32,19 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class HeatMapFragment extends Fragment implements AdapterView.OnItemSelectedListener, OnMapReadyCallback {
     private DatabaseReference mDatabase;
     private ArrayList<Results> results = new ArrayList<Results>();
-    private final String[] locations = {"Library", "Canteen", "Kiosk", "Airport", "ABD", "Garden"};
+    private final Map<String, LatLng[]> locationDict = new HashMap<String, LatLng[]>() {{
+        put("Library", new LatLng[]{new LatLng(0, 0), new LatLng(0, 0), new LatLng(0, 0), new LatLng(0, 0)});
+        put("Canteen", new LatLng[]{new LatLng(0, 0), new LatLng(0, 0), new LatLng(0, 0), new LatLng(0, 0)});
+        put("Kiosk", new LatLng[]{new LatLng(0, 0), new LatLng(0, 0), new LatLng(0, 0), new LatLng(0, 0)});
+        put("Airport", new LatLng[]{new LatLng(0, 0), new LatLng(0, 0), new LatLng(0, 0), new LatLng(0, 0)});
+        put("ABD", new LatLng[]{new LatLng(0, 0), new LatLng(0, 0), new LatLng(0, 0), new LatLng(0, 0)});
+        put("Garden", new LatLng[]{new LatLng(0, 0), new LatLng(0, 0), new LatLng(0, 0), new LatLng(0, 0)});
+    }};
 
     public HeatMapFragment() {
         // Required empty public constructor
@@ -59,10 +58,9 @@ public class HeatMapFragment extends Fragment implements AdapterView.OnItemSelec
         super.onCreate(savedInstanceState);
 
         getActivity().setContentView(R.layout.activity_main);
-        /*SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager()
-               .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);*/
-
+        //SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager()
+        //       .findFragmentById(R.id.map);
+        //mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -107,11 +105,15 @@ public class HeatMapFragment extends Fragment implements AdapterView.OnItemSelec
         final LatLng PSHS = new LatLng(7.082788894235911, 125.50813754841627);
         googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(PSHS));
+
+        for(LatLng[] ll: locationDict.values()){
+            Polygon polygon = googleMap.addPolygon(new PolygonOptions()
+                    .add(ll));
+        }
     }
 
-    public void onItemSelected(AdapterView<?> parent, View view,
-                               int pos, long id) {
-        updateHeatMap(pos);
+    public void onItemSelected(AdapterView<?> parent, View view, int metric, long id) {
+        updateHeatMap(metric);
     }
 
     public void onNothingSelected(AdapterView<?> parent){
@@ -124,7 +126,7 @@ public class HeatMapFragment extends Fragment implements AdapterView.OnItemSelec
         return (int) ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
     }
 
-    private double getDLMean(List<Double> doubleList){
+    private double getMeanResult(List<Double> doubleList){
         if(!doubleList.isEmpty()){
             double sum = 0.0;
             for (Double d : doubleList) {
@@ -135,16 +137,17 @@ public class HeatMapFragment extends Fragment implements AdapterView.OnItemSelec
         return 0.0;
     }
 
-    private double getDLMin(List<Double> doubleList){
+    private double getMinResult(List<Double> doubleList){
         return doubleList.indexOf(Collections.min(doubleList));
     }
 
-    private double getDLMax(List<Double> doubleList){
+    private double getMaxResult(List<Double> doubleList){
         return doubleList.indexOf(Collections.max(doubleList));
     }
 
-    public void updateHeatMap(int metric){
-        for(String location: locations) {
+    public List<Integer> updateHeatMap(int metric){
+        List<Integer> colorList = new ArrayList<>();
+        for(String location: locationDict.keySet()) {
             List<Double> resultsList = new ArrayList<>();
             for (Results curResult : results) {
                 double intensity = 0;
@@ -175,10 +178,12 @@ public class HeatMapFragment extends Fragment implements AdapterView.OnItemSelec
                 resultsList.add(intensity);
             }
 
-            double meanResult = getDLMean(resultsList);
+            double meanResult = getMeanResult(resultsList);
             //chooses color from meanResult
-            int color = map(meanResult, getDLMin(resultsList), getDLMax(resultsList), 0, 11);
+            int color = map(meanResult, getMinResult(resultsList), getMaxResult(resultsList), 0, 11);
+            colorList.add(color);
         }
+        return colorList;
     }
 
     public void resetHeatMap(){
