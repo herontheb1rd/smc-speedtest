58cc74351f2448778ef61594a44fcc040cfa4b45
Heron Nalasa <herongabriel.nalasa@smc.pshs.edu.ph>
2024-01-28 22:46:56 +0800

    Added code to add markers of where QR codes are located

--- app/src/main/java/com/herontheb1rd/smcspeedtest/HeatMapFragment.java
+++ app/src/main/java/com/herontheb1rd/smcspeedtest/HeatMapFragment.java
@@ -17,6 +17,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.firebase.database.DataSnapshot;
@@ -45,7 +46,8 @@ public class HeatMapFragment extends Fragment implements AdapterView.OnItemSelec
         put("Garden", new LatLng[]{new LatLng(0, 0), new LatLng(0, 0), new LatLng(0, 0), new LatLng(0, 0)});
     }};
     private final int[] colorGradient = {0x0066ff000, 0xff66ff00, 0xff93ff00, 0xffc1ff00, 0xffeeff00, 0xfff4e30, 0xfff9c600, 0xffffaa00, 0xffff7100, 0xffff3900, 0xffff0000};
-    private final LatLng PSHS = new LatLng(7.082788894235911, 125.50813754841627);
+    private final LatLng pshsLatLng = new LatLng(7.082788894235911, 125.50813754841627);
+    private final LatLng[] qrLatLng = {new LatLng(0, 0), new LatLng(0,0)};
 
     private ArrayList<Polygon> mPolygonList = new ArrayList<>();
 
@@ -90,7 +92,11 @@ public class HeatMapFragment extends Fragment implements AdapterView.OnItemSelec
     @Override
     public void onMapReady(GoogleMap googleMap) {
         googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(PSHS));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(pshsLatLng));
+
+        for(LatLng ll: qrLatLng){
+            googleMap.addMarker(new MarkerOptions().position(ll));
+        }
 
         for(LatLng[] ll: locationDict.values()){
             Polygon polygon = googleMap.addPolygon(new PolygonOptions()
